b533f9de75c7362182ff5b89afed3145da2b7fc0
dbstj0403 <wcn09088@naver.com>
2025-08-07 23:01:08 +0900

    add: spring security config 파일 & index.html 추가

--- src/main/java/com/example/hanaro/config/SecurityConfig.java
+++ src/main/java/com/example/hanaro/config/SecurityConfig.java
@@ -1,15 +1,42 @@
 package com.example.hanaro.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity // Spring Security 기능을 활성화
+@RequiredArgsConstructor // final 필드가 있는 경우 생성자를 자동 생성해주는 Lombok 어노테이션
 public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean // Spring Security의 보안 필터 체인 설정
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .formLogin(form -> form.disable())
+            .httpBasic(basic -> basic.disable())
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers(
+                            "/",
+                            "/index.html",
+                            "/static/**",
+                            "/api/user/join",
+                            "/v3/api-docs/**",
+                            "/swagger-ui/**",
+                            "/swagger-ui.html").permitAll()
+                    .anyRequest().authenticated()
+            );
+
+        return http.build();
     }
 }
