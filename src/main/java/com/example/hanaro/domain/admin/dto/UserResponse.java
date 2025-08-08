99dc56627d63fa05eaf42de54f5e8a5ca2f0d30b
dbstj0403 <wcn09088@naver.com>
2025-08-08 09:52:47 +0900

    chore: dto schema 추가

--- src/main/java/com/example/hanaro/domain/admin/dto/UserResponse.java
+++ src/main/java/com/example/hanaro/domain/admin/dto/UserResponse.java
@@ -1,12 +1,19 @@
 package com.example.hanaro.domain.admin.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class UserResponse {
+
+    @Schema(description = "ID", example = "1")
     private Long id;
+
+    @Schema(description = "Name", example = "별돌이")
     private String username;
+
+    @Schema(description = "Email", example = "hanaro@email.com")
     private String email;
 }
--- src/main/java/com/example/hanaro/domain/user/dto/UserJoinRequest.java
+++ src/main/java/com/example/hanaro/domain/user/dto/UserJoinRequest.java
@@ -2,6 +2,7 @@ package com.example.hanaro.domain.user.dto;
 
 import com.example.hanaro.domain.user.entity.User;
 import com.example.hanaro.domain.user.entity.User.Role;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -13,15 +14,18 @@ public class UserJoinRequest {
 
     @NotBlank(message = "이메일은 필수입니다.")
     @Email(message = "올바른 이메일 형식이 아닙니다.")
+    @Schema(description = "Email", example = "hanaro@email.com")
     private String email;
 
     @NotBlank(message = "비밀번호는 필수입니다.")
     @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하여야 합니다.")
+    @Schema(description = "Password", example = "12345678")
     private String password;
 
     @NotBlank(message = "닉네임은 필수입니다.")
     @Size(min = 2, max = 10, message = "닉네임은 2자 이상 10자 이하여야 합니다.")
     @Pattern(regexp = "^[가-힣a-zA-Z0-9]+$", message = "닉네임은 한글, 영문, 숫자만 사용할 수 있습니다.")
+    @Schema(description = "Name", example = "별돌이")
     private String name;
 
     public User toEntity(String encodedPassword) {
